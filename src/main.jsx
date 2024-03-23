27eb7f6aac6234e45f53cf2acb39610f142f8b39
arfanur04 <arfanur04@gmail.com>
2024-03-23 12:21:19 +0600

    45-2

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <div>Hello from react router</div>,
-	},
-	{
-		path: "/about",
-		element: <div>I am in the about page</div>,
-	},
-	{
-		path: "/contact",
-		element: <div>Call me right now</div>,
+		element: <Home></Home>,
+		children: [
+			{
+				path: "/about",
+				element: <About></About>,
+			},
+			{
+				path: "/contact",
+				element: <Contact></Contact>,
+			},
+		],
 	},
 ]);
 
