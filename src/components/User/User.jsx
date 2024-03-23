a260f5e5ffa02248c89246c0b1811684e16762c3
arfanur04 <arfanur04@gmail.com>
2024-03-23 21:43:47 +0600

    45-5

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const User = ({ user }) => {
-	const { name, email, phone } = user;
+	const { id, name, email, phone } = user;
 	const userStyle = {
 		border: "2px solid yellow",
 		padding: "5px",
@@ -11,8 +12,12 @@ const User = ({ user }) => {
 	return (
 		<div style={userStyle}>
 			<h2>{name}</h2>
-			<p>{email}</p>
-			<p>{phone}</p>
+			<p>email: {email}</p>
+			<p>phone: {phone}</p>
+			<Link to={`/user/${id}`}>Show Details</Link>
+			<Link to={`/user/${id}`}>
+				<button>Click Me</button>
+			</Link>
 		</div>
 	);
 };
