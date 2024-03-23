81a8d61feaa89543dd043ae048c90132f9f622e2
arfanur04 <arfanur04@gmail.com>
2024-03-23 17:16:35 +0600

    45-4

---
+++ src/components/User/User.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+const User = ({ user }) => {
+	const { name, email, phone } = user;
+	const userStyle = {
+		border: "2px solid yellow",
+		padding: "5px",
+		borderRadius: "20px",
+	};
+
+	return (
+		<div style={userStyle}>
+			<h2>{name}</h2>
+			<p>{email}</p>
+			<p>{phone}</p>
+		</div>
+	);
+};
+
+User.propTypes = {
+	user: PropTypes.object,
+};
+
+export default User;
