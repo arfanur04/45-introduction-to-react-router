9cea2b16d361649062c8af7cf6adec397a17f500
arfanur04 <arfanur04@gmail.com>
2024-03-23 16:41:15 +0600

    45-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
+import App from "./App.jsx";
+import Users from "./components/Users/Users.jsx";
 
 const router = createBrowserRouter([
 	{
@@ -20,13 +21,16 @@ const router = createBrowserRouter([
 				path: "/contact",
 				element: <Contact></Contact>,
 			},
+			{
+				path: "/users",
+				element: <Users></Users>,
+			},
 		],
 	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		{/* <App /> */}
 		<RouterProvider router={router}></RouterProvider>
 	</React.StrictMode>
 );
