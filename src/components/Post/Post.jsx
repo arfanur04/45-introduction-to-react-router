d7047b10d8a9fcd2a6aab35d36bdce7d622c309a
arfanur04 <arfanur04@gmail.com>
2024-03-24 21:21:23 +0600

    45-6

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Post = ({ post }) => {
+	const { id, title } = post;
+
+	const postStyle = {
+		border: "2px solid yellow",
+		padding: "5px",
+		borderRadius: "20px",
+	};
+
+	return (
+		<div style={postStyle}>
+			<h4>Post of Id: {id}</h4>
+			<p>Title: {title}</p>
+			<Link to={`/post/${id}`}>Post Detail</Link>
+			<Link to={`/post/${id}`}>
+				<button>Show Details</button>
+			</Link>
+		</div>
+	);
+};
+
+Post.propTypes = {
+	post: PropTypes.object,
+};
+
+export default Post;
